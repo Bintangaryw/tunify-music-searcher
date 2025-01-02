79d84423c1cbaabe41a1359557a57c2c958fb7d7
bintang ary <wardhanabintang07@gmail.com>
2025-01-02 19:14:40 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "../src/pages/landingpage/index";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/landingpage/index.jsx
@@ -0,0 +1,9 @@
+const index = () => {
+    return (
+        <>
+            <div className="container"></div>
+        </>
+    );
+};
+
+export default index;

6b437bdf0e2c4005290f73fba71af241f66eee89
bintang ary <wardhanabintang07@gmail.com>
2025-01-04 11:54:15 +0700

    Add artist card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../src/pages/landingpage/index";
 import Dashboard from "../src/pages/dashboard/index";
 import Card from "../src/components/album-card/index";
+import ArtistCard from "../src/components/artist-card/index";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/card-album" element={<Card />} />
+                    <Route path="/card-artist" element={<ArtistCard />} />
                 </Routes>
             </BrowserRouter>
         </>
--- src/components/album-card/index.jsx
+++ src/components/album-card/index.jsx
@@ -1,12 +1,12 @@
 const index = () => {
     return (
         <>
-            <div className="max-w-sm mx-auto bg-[#323232] text-white rounded-xl shadow-md overflow-hidden">
-                {/* Gambar Album */}
-                <img className="w-full h-64 object-cover" src="https://via.placeholder.com/300" alt="Album Art" />
+            <div className="max-w-md mx-auto bg-[#323232] text-white rounded-xl shadow-md flex">
+                {/* Gambar */}
+                <img className="w-64 h-64 object-cover rounded-xl m-6" src="https://via.placeholder.com/300" alt="Album Art" />
 
-                {/* Detail Album */}
-                <div className="p-4">
+                {/* Detail */}
+                <div className="p-4 flex flex-col justify-center">
                     <h2 className="text-lg font-bold">Urban Sunset</h2>
                     <p className="text-gray-400">Artist: The City Lights</p>
 

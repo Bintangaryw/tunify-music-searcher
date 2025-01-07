a8a734f99561814451e26cdb899b28286cdcbdf6
bintang ary <wardhanabintang07@gmail.com>
2025-01-07 11:37:11 +0700

    Hit search API

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,11 +3,42 @@ import Navbar from "../../components/navbar/index";
 import AlbumCard from "../../components/album-card/index";
 import ArtistCard from "../../components/artist-card/index";
 import TrackCard from "../../components/track-card/index";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
 
-const index = () => {
+const Index = () => {
+    // const [useSearchParams]
+    const [searchedArtists, setSearchedArtists] = useState([]);
+    const [searchedAlbums, setSearchedAlbums] = useState([]);
+    const [searchedTracks, setSearchedTracks] = useState([]);
+
+    useEffect(() => {
+        const getSearchedItems = async () => {
+            try {
+                const response = await axios.get("https://api.spotify.com/v1/search", {
+                    params: {
+                        q: "Avenged Sevenfold",
+                        type: "artist,album,track",
+                        market: "ID",
+                        limit: 15,
+                    },
+                    headers: {
+                        Authorization: "Bearer BQAo7LhBq2hK05U9Mi6nb_ZqGcBMHN6069gwMByuHr2ziBvseaGK8d8kz0Sa_CXhTy7yJLH2fvN0x7OULnlOUX6A15TFx5PRSKj_lntT2CKfD0arcg0",
+                    },
+                });
+                const fetched_items = response.data;
+                console.log(fetched_items);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getSearchedItems();
+    }, []);
+
     return (
         <>
-            <div className="flex p-8 max-w-[1920px] mx-auto">
+            <div className="flex p-8 max-w-[1700px] mx-auto">
                 <div>
                     <Sidebar />
                 </div>
@@ -22,7 +53,7 @@ const index = () => {
                             {/* Artists */}
                             <div>
                                 <p className="text-white text-4xl font-bold pl-5 pt-5 pb-3">Artists</p>
-                                <div className="grid grid-cols-4 lg:grid-cols-5 gap-4 ">
+                                <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 ">
                                     <ArtistCard />
                                     <ArtistCard />
                                     <ArtistCard />
@@ -65,4 +96,4 @@ const index = () => {
     );
 };
 
-export default index;
+export default Index;
